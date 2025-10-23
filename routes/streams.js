839faa8bbee9c1223177343678d9e20bfdb2c4b7
drbilel35-sudo <drbilel35@gmail.com>
2025-10-23 18:52:19 +0400

    Update streams.js

--- routes/streams.js
+++ routes/streams.js
@@ -5,7 +5,11 @@ const {
   getStream,
   getActiveStreams,
   getStreamStats,
-  updateStreamSettings
+  updateStreamSettings,
+  joinStream,
+  leaveStream,
+  recordStream,
+  getStreamHealth
 } = require('../controllers/streamController');
 const { auth, checkPermission } = require('../middleware/auth');
 
@@ -20,6 +24,14 @@ router.post('/:cameraId/start', checkPermission('view_streams'), startStream);
 router.post('/:streamKey/stop', checkPermission('view_streams'), stopStream);
 router.get('/:streamKey', checkPermission('view_streams'), getStream);
 router.get('/:streamKey/stats', checkPermission('view_streams'), getStreamStats);
+router.get('/:streamKey/health', checkPermission('view_streams'), getStreamHealth);
 router.put('/:streamKey/settings', checkPermission('manage_streams'), updateStreamSettings);
+
+// Viewer management
+router.post('/:streamKey/join', checkPermission('view_streams'), joinStream);
+router.post('/:streamKey/leave', checkPermission('view_streams'), leaveStream);
+
+// Recording
+router.post('/:streamKey/record', checkPermission('manage_streams'), recordStream);
 
 module.exports = router;
